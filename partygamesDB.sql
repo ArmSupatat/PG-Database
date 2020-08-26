2812a6ec93ea014b1cdc22d09f75104bc0e7da33
Supatat Singkomfu <Supatat Singkomfu>
2020-08-26 11:33:02 +0700

    Edit DB

--- partygamesDB.sql
+++ partygamesDB.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE IN NOT EXISTS `partygamesDB`;
+CREATE DATABASE IF NOT EXISTS `partygamesDB`;
 
 USE `partygamesDB`;
 
-DROP TABLE IF EXISTS `Profiles`;
+DROP TABLE IF EXISTS `User_join_Members`;
+DROP TABLE IF EXISTS `Comments`;
 DROP TABLE IF EXISTS `Posts`;
-DROP TABLE IF EXISTS `comments`;
-DROP TABLE IF EXISTS `Client_join_member`;
-DROP TABLE IF EXISTS `Member`;
+DROP TABLE IF EXISTS `Members`;
+DROP TABLE IF EXISTS `Users`;
 
 CREATE TABLE `Users`(
     `user_id` BIGINT AUTO_INCREMENT,
@@ -31,9 +31,9 @@ CREATE TABLE `Posts`(
     `letter` varchar(150) NOT NULL,
 
     PRIMARY KEY (`post_id`)
-    CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Posts`.`post_id`,
-    CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`comment_id`) REFERENCES `Posts`.`post_id`,
-    CONSTRAINT `member_ibfk_3` FOREIGN KEY (`member_id`) REFERENCES `Posts`.`post_id`,
+    CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users`.`user_id`,
+    CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`comment_id`) REFERENCES `Comments`.`comment_id`,
+    CONSTRAINT `member_ibfk_3` FOREIGN KEY (`member_id`) REFERENCES `Members`.`member_id`,
 ) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
 
 CREATE TABLE `Comments`(
@@ -42,13 +42,15 @@ CREATE TABLE `Comments`(
     `timestamp` timestamp,
 
     PRIMARY KEY (`comment_id`)
-    CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Comments`.`comment_id`,
+    CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users`.`user_id`,
 ) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
 
 CREATE TABLE `User_join_Members`(
-    `` BIGINT AUTO_INCREMENT,
+    `member_id` BIGINT AUTO_INCREMENT,
+    `user_id` BIGINT AUTO_INCREMENT,
 
-    PRIMARY KEY (``)
-    CONSTRAINT `member_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `User_join_Members`.``,
-    CONSTRAINT `user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `User_join_Members`.``,
+    PRIMARY KEY (`member_id`)
+    PRIMARY KEY (`user_id`)
+    CONSTRAINT `member_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `Members`.`member_id`,
+    CONSTRAINT `user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `Users`.`user_id`,
 ) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
