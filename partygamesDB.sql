1731a41a95a5876b670b752bf7bd50608d70a10a
Supatat Singkomfu <Supatat Singkomfu>
2020-08-31 02:28:14 +0700

    Update sql database

--- partygamesDB.sql
+++ partygamesDB.sql
@@ -2,9 +2,9 @@ CREATE DATABASE IF NOT EXISTS `partygamesDB`;
 
 USE `partygamesDB`;
 
+DROP TABLE IF EXISTS `Posts`;
 DROP TABLE IF EXISTS `User_join_Members`;
 DROP TABLE IF EXISTS `Comments`;
-DROP TABLE IF EXISTS `Posts`;
 DROP TABLE IF EXISTS `Members`;
 DROP TABLE IF EXISTS `Users`;
 
@@ -23,26 +23,13 @@ CREATE TABLE `Members`(
     PRIMARY KEY (`member_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
 
-CREATE TABLE `Posts`(
-    `post_id` BIGINT AUTO_INCREMENT,
-    `title` varchar(50) NOT NULL,
-    `timestamp` timestamp,
-    `details` varchar(150),
-    `letter` varchar(150) NOT NULL,
-
-    PRIMARY KEY (`post_id`)
-    CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users`.`user_id`,
-    CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`comment_id`) REFERENCES `Comments`.`comment_id`,
-    CONSTRAINT `member_ibfk_3` FOREIGN KEY (`member_id`) REFERENCES `Members`.`member_id`,
-) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
-
 CREATE TABLE `Comments`(
     `comment_id` BIGINT AUTO_INCREMENT,
     `comment` varchar(100),
     `timestamp` timestamp,
 
     PRIMARY KEY (`comment_id`)
-    CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users`.`user_id`,
+    CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`),
 ) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
 
 CREATE TABLE `User_join_Members`(
@@ -51,6 +38,19 @@ CREATE TABLE `User_join_Members`(
 
     PRIMARY KEY (`member_id`)
     PRIMARY KEY (`user_id`)
-    CONSTRAINT `member_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `Members`.`member_id`,
-    CONSTRAINT `user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `Users`.`user_id`,
+    CONSTRAINT `member_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `Members` (`member_id`),
+    CONSTRAINT `user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`),
+) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
+
+CREATE TABLE `Posts`(
+    `post_id` BIGINT AUTO_INCREMENT,
+    `title` varchar(50) NOT NULL,
+    `timestamp` timestamp,
+    `details` varchar(150),
+    `letter` varchar(150) NOT NULL,
+
+    PRIMARY KEY (`post_id`)
+    CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`),
+    CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`comment_id`) REFERENCES `Comments` (`comment_id`),
+    CONSTRAINT `member_ibfk_3` FOREIGN KEY (`member_id`) REFERENCES `Members` (`member_id`),
 ) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
