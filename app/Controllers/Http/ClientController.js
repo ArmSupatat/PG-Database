f3c6f952bda637706e13f79acd10b1d97f4f051d
Ajarnnotes <ajarnnotes@gmail.com>
2020-09-11 15:56:50 +0700

    add dependencies

--- app/Controllers/Http/ClientController.js
+++ app/Controllers/Http/ClientController.js
@@ -1,6 +1,10 @@
 'use strict'
+const Database = use('Database')
+const Validator = use('Validator')
+const Teacher = use('App/Models/Teacher')
 
 class ClientController {
+
 }
 
 module.exports = ClientController
