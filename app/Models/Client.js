80589a1d4bf35f5a36d8ad54c32bcfd1ee96d74f
ArmSupatat <supatat@artisan.co.th>
2020-09-11 16:48:05 +0700

    Create models files

---
+++ app/Models/Client.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Client extends Model {
+    static get primaryKey() {
+        return 'user_id'
+    }
+    
+    posts() {
+        return this.hasMany('App/Models/Post');
+    }
+
+    comments() {
+        return this.hasMany('App/Models/Comment');
+    }
+}
+
+module.exports = Client
---
+++ app/Models/Post.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Post extends Model {
+    static get primaryKey() {
+        return 'post_id'
+    }
+
+    clients() {
+        return this.belongsTo('App/Models/Client')
+    }
+
+    comments() {
+        return this.belongsTo('App/Models/Comment')
+    }
+}
+
+module.exports = Post
