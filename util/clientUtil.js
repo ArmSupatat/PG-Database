8ddfa7f27af85b8257d56442162524fd5f8ac064
Ajarnnotes <ajarnnotes@gmail.com>
2020-09-11 17:52:13 +0700

    add clientUtil

---
+++ util/clientUtil.js
@@ -0,0 +1,29 @@
+class ClienttUtil {
+    constructor(ClientModel){
+        this._Client = ClientModel
+    }
+
+    async getAll (references){
+        const clients = this._Client.query()
+
+        if(references){
+            const extractedReferences = references.split(",")
+            clients.with(extractedReferences)
+        }
+
+        return clients.fetch()
+    }
+
+    getById(references){
+        const client = this._Client.query()
+
+        if (references){
+            const extractedReferences = references.split(",")
+            client.with(extractedReferences)
+        }
+
+        return client.where('subject_id', clientId).fetch()
+    }
+}
+
+module.exports = ClienttUtil
