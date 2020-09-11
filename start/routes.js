bfa3f888f851e65811224b5dbe4dda893f059bc0
ArmSupatat <supatat@artisan.co.th>
2020-09-11 15:25:44 +0700

    Edit routes

--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,31 @@ const Route = use('Route')
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
+
+Route.group(() => {
+
+  // Route.resource("/clients", "ClientController")
+  Route.get('/clients', 'ClientController.index')
+  Route.get('/clients/:id', 'ClientController.index')
+  Route.post('/clients', 'ClientController.store')
+  // Route.put('/clients/:id', 'ClientController.update')
+  // Route.patch('/clients/:id', 'ClientController.update')
+  // Route.delete('/clients:id', 'ClientController.destroy')
+
+  // Route.resource("/posts", "PostController")
+  Route.get('/posts', 'PostController.index')
+  Route.get('/posts/:id', 'PostController.index')
+  Route.post('/posts', 'PostController.store')
+  // Route.put('/posts/:id', 'PostController.update')
+  // Route.patch('/posts/:id', 'PostController.update')
+  // Route.delete('/posts:id', 'PostController.destroy')
+
+  // Route.resource("/comments", "commentController")
+  Route.get('/comments', 'CommentController.index')
+  Route.get('/comments/:id', 'CommentController.index')
+  Route.post('/comments', 'CommentController.store')
+  // Route.put('/comments/:id', 'CommentController.update')
+  // Route.patch('/comments/:id', 'CommentController.update')
+  // Route.delete('/comments:id', 'CommentController.destroy')
+
+})
