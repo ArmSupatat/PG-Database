0bfa7235454721d3166af031aee4126302961eb3
Supatat Singkomfu <Supatat Singkomfu>
2020-09-13 03:35:35 +0700

    Edit seeds

--- database/factory.js
+++ database/factory.js
@@ -27,20 +27,20 @@ Factory.blueprint('App/Models/Client', (faker) => {
         username: faker.username(),
         password: faker.password(),
         email: faker.email(),
-        contact: faker.contact()
+        contact: faker.phone()
     }
 })
 
 Factory.blueprint('App/Models/Post', (faker) => {
     return {
         party_size: faker.integer({ min: -5, max: 5 }),
-        title: faker.(),
-        details: faker.()
+        title: faker.word(),
+        details: faker.paragraph()
     }
 })
 
 Factory.blueprint('App/Models/Comment', (faker) => {
     return {
-        comment: faker.()
+        comment: faker.sentence()
     }
 })
