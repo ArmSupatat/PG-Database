d866a95a70745d1a50d3e2057487e26516ad717b
Supatat Singkomfu <Supatat Singkomfu>
2020-09-15 03:45:54 +0700

    Edit join

--- app/Models/Join.js
+++ app/Models/Join.js
@@ -4,6 +4,25 @@
 const Model = use('Model')
 
 class Join extends Model {
+    static get primaryKey() {
+        return 'join_id'
+    }
+
+    static get createdAtColumn(){
+        return null
+    }
+
+    static get updatedAtColumn(){
+        return null
+    }
+
+    clients() {
+        return this.belongsTo('App/Models/Client');
+    }
+
+    posts() {
+        return this.belongsTo('App/Models/Post');
+    }
 }
 
 module.exports = Join
