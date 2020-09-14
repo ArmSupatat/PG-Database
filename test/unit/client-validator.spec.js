99cd0da2c9c5a399e5852505b169d6278cc8b724
Ajarnnotes <ajarnnotes@gmail.com>
2020-09-15 05:17:17 +0700

    create ClientValidator test

---
+++ test/unit/client-validator.spec.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { test } = use('Test/Suite')('Client Validator')
+const clientValidator = require('../../service/ClientValidator')
+
+test('should return an error when enter wrong email', async ({ assert }) => {
+  const validatedData = await clientValidator({
+    username: "Johny",
+    email: "Wrong mail",
+    password: "12345678",
+    contact: "My discord is Johnthesheep"
+  })
+  assert.equal(validatedData.error.length, 1)
+})
+
+test('should return an error when password does not reach minimum 8 digit', async ({ assert }) => {
+  const validatedData = await clientValidator({
+    username: "Johny",
+    email: "right@email.com",
+    password: "12345",
+    contact: "My discord is Johnthesheep"
+  })
+  assert.equal(validatedData.error.length, 1)
+})
