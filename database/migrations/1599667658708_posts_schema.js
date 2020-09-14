ae484182cf63f61676cd49313e3d380606c397b3
Supatat Singkomfu <Supatat Singkomfu>
2020-09-15 01:05:49 +0700

    Edit posts migration

--- database/migrations/1599667658708_posts_schema.js
+++ database/migrations/1599667658708_posts_schema.js
@@ -8,14 +8,12 @@ class PostsSchema extends Schema {
     this.create('posts', (table) => {
       table.increments('post_id', 5)
       table.integer("party_size", 3)
-      table.string("title", 50)
-      .notNullable()
+      table.string("title", 50).notNullable()
       table.string("details", 150)
       table.timestamp("user_timestamps").default(this.fn.now())
       table.timestamps('')
     
-      table.integer('user_id', 5)
-      .unsigned()
+      table.integer('user_id', 5).unsigned()
       table.foreign('user_id')
       .references('clients.user_id')
       .onDelete('CASCADE') 
