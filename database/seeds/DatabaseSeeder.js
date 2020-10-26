540cbc0f8be43a30a7d5fe4409c5da31cb8ad76b
Supatat Singkomfu <Supatat Singkomfu>
2020-10-27 04:49:43 +0700

    Edit Database Seeder

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -1,7 +1,5 @@
 'use strict'
-
 const ClientsSchema = require('../migrations/1599667626995_clients_schema')
-
 /*
 |--------------------------------------------------------------------------
 | DatabasisSeeder
@@ -11,30 +9,22 @@ const ClientsSchema = require('../migrations/1599667626995_clients_schema')
 | make use of Lucid models directly.
 |
 */
-
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
 const Client = use('App/Models/Client')
 const Post = use('App/Models/Post')
-
 class DatabasisSeeder {
   async run() {
-
-    const clients = await Factory.model('App/Models/Client').createMany(10)
-
+    const clents = await Factory.model('App/Models/Client').createMany(10)
+    const posts = await Factory.model('App/Models/Post').createMany(10)
     const comments = await Factory.model('App/Models/Comment').createMany(10)
-
     // const UserIds = await Client
     //   .find()
-
     // const PostIds = await Post
     //   .find()
-
     const joins = await Factory.model('App/Model/Join').makeMany(10)
-
     let currentJoinIndex = 0;
     const clientPerPost = 2;
-
     for (const join of joins) {
       const selectedPosts = clients.slice(
         currentJoinIndex,
@@ -43,12 +33,9 @@ class DatabasisSeeder {
       await join
         .posts()
         .saveMany(selectedPosts)
-
         currentJoinIndex += clientPerPost
     }
 
-
   }
 }
-
 module.exports = DatabasisSeeder
